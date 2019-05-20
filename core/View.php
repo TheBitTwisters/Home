2c1b4d43f1679742d593e00894744d471827b6d3
Michael Angelo <michael01angelo@gmail.com>
2019-05-20 16:49:04 +0800

    View to handle the rendering

---
+++ core/View.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Home;
+/**
+ * Class View
+ * The part that handles all the output
+ */
+class View
+{
+    /**
+     * simply includes (=shows) the view. this is done from the controller. In the controller, you usually say
+     * $this->view->render('help/index'); to show (in this example) the view index.php in the folder help.
+     * Usually the Class and the method are the same like the view, but sometimes you need to show different views.
+     * @param string $filename Path of the to-be-rendered view, usually folder/file(.php)
+     * @param array $data Data to be used in the view
+     */
+    public function render($filename, $data = null)
+    {
+        if ($data) {
+            foreach ($data as $key => $value) {
+                $this->{$key} = $value;
+            }
+        }
+        $filename = explode('/', $filename);
+        $app_name = ucwords($filename[0]);
+        $file = $filename[1];
+
+        require Config::get('PATH_APPS') . $app_name . '/templates/' . $file . '.php';
+    }
+
+    /**
+     * Renders pure JSON to the browser, useful for API construction
+     * @param $data
+     */
+    public function renderJSON($data)
+    {
+        header("Content-Type: application/json");
+        echo json_encode($data);
+    }
+
+    /**
+     * Converts characters to HTML entities
+     * This is important to avoid XSS attacks, and attempts to inject malicious code in your page.
+     *
+     * @param  string $str The string.
+     * @return string
+     */
+    public function encodeHTML($str)
+    {
+        return htmlentities($str, ENT_QUOTES, 'UTF-8');
+    }
+
+}
