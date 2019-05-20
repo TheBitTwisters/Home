ffbedfe4ccc0cf8c677de76a470528c0710c2f4d
Michael Angelo <michael01angelo@gmail.com>
2019-05-20 16:48:53 +0800

    All session features

---
+++ core/Session.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Home;
+class Session
+{
+    public static function init()
+    {
+        if (session_id() == '') session_start();
+    }
+
+    public static function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public static function unset($key) {
+        if (isset($_SESSION[$key])) unset($_SESSION[$key]);
+    }
+
+    public static function get($key)
+    {
+        if (isset($_SESSION[$key])) {
+            $value = $_SESSION[$key];
+            return Filter::XSSFilter($value);
+        }
+        return false;
+    }
+
+    public static function add($key, $value)
+    {
+        $_SESSION[$key][] = $value;
+    }
+
+    public static function destroy()
+    {
+        session_destroy();
+        session_start();
+    }
+
+    public static function isUserLoggedIn()
+    {
+        return (self::get('user_id') > 0);
+    }
+
+    public static function updateSession()
+    {
+        // if (self::isUserLoggedIn())
+        //     LoginModel::updateActiveTimestamp();
+    }
+}
