e0ba7794b738ec06e928d7f29939b3fb78214f34
Michael Angelo <michael01angelo@gmail.com>
2019-05-22 07:13:53 +0800

    Error app

--- apps/Error/Controller.php
+++ apps/Error/Controller.php
@@ -9,10 +9,32 @@ class Controller extends \Home\BaseController
         parent::__construct();
     }
 
+    public function index($code = 0)
+    {
+        if ($code) {
+            $data['code'] = $code;
+            $this->View->render('error/code', $data);
+        } else {
+            \Home\Redirect::home();
+        }
+        return true;
+    }
+
     public function not_found()
 	{
 		$this->View->render('error/404');
 		return true;
+	}
+
+    public function db($code = 0)
+	{
+        if ($code) {
+            $data['code'] = $code;
+            $this->View->render('error/db', $data);
+        } else {
+            \Home\Redirect::home();
+        }
+        return true;
 	}
 
 }
---
+++ apps/Error/Model.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Error;
+class Model extends \Home\BaseModel
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}
