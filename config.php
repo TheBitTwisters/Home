d90e44ab823ad7c15dd9564e7b1e86d82575aa46
Michael Angelo <michael01angelo@gmail.com>
2019-05-21 16:54:32 +0800

    Fix core Config

--- config.php
+++ config.php
@@ -2,68 +2,12 @@
 
 $base_url = 'test.kpoparazzi.com';
 
-/**
- * Returns the full configuration.
- * This is used by the core/Config class.
- */
- return array(
+return array(
 
-     //
-     'MAINTENANCE' => false,
+    'URL' => 'https://'.$base_url.'/',
+    'URL_ROOT' => 'https://'.$base_url,
+    'URL_PLUGINS' => 'https://'.$base_url.'/plugins/',
 
- 	// URL
-     'URL' => 'https://'.$base_url.'/',
-     'URL_ROOT' => 'https://'.$base_url,
+    'COOKIE_DOMAIN' => $base_url,
 
-     // META
-     'SITE_LOCALE' => 'en_US',
-     'SITE_NAME' => 'KPOParazzi.com',
-     'SITE_AUTHOR' => 'The Bit Twisters',
-     'SITE_CAPTION' => 'KPOParazzi.com - Community for the KPOP &amp; KDRAMA fans',
-     'SITE_TITLE' => 'KPOParazzi.com',
-     'SITE_DESCRIPT' => 'Community for the KPOP &amp; KDRAMA fans',
-     'SITE_KEYWORDS' => 'kpop, korea, kdrama, kpoparazzi, latest, news, quiz, fun, interactive, k, gossip, story',
-     'SITE_LOGO' => 'https://'.$base_url.'/img/kpoparazzi.png',
-     'SITE_PREVIEW' => 'https://'.$base_url.'/img/k-preview.jpg',
-     'SITE_TERMS' => 'https://'.$base_url.'/about/terms/',
-     'SITE_PRIVACY' => 'https://'.$base_url.'/about/privacy/',
-
-     // PATH
-     'PATH_MODEL' => realpath(dirname(__FILE__).'/../../') . '/application/model/',
-     'PATH_VIEW' => realpath(dirname(__FILE__).'/../../') . '/application/view/',
-     'PATH_CONTROLLER' => realpath(dirname(__FILE__).'/../../') . '/application/controller/',
-     'PATH_FILES' => realpath(dirname(__FILE__).'/../../') . '/public/',
-     'PATH_AVATAR' => realpath(dirname(__FILE__).'/../../') . '/public/avatar/',
-     'PATH_ERROR_LOG' => realpath(dirname(__FILE__).'/../../../') . '/logs/error_www_php.log',
-     'PATH_BACKDROP' => realpath(dirname(__FILE__).'/../../') . '/public/img/backdrop/',
-     'PATH_FONTS' => realpath(dirname(__FILE__).'/../../') . '/public/fonts/',
-
-     // APP & ACTN
-     'DEFAULT_APP' => 'home',
-     'DEFAULT_ACTION' => 'index',
-
-     // DB
-     'DB_TYPE' => 'mysql',
-     'DB_HOST' => '*',
-     'DB_NAME' => 'test',
-     'DB_USER' => 'admin',
-     'DB_PASS' => 'UndefinedPassword',
-     'DB_PORT' => '3306',
-     'DB_CHARSET' => 'utf8mb4',
-
-     // COOKIE
-     'COOKIE_RUNTIME' => 604800,
-     'COOKIE_PATH' => '/',
-     'COOKIE_DOMAIN' => $base_url,
-     'COOKIE_SECURE' => true,
-     'COOKIE_HTTP' => true,
-     'SESSION_RUNTIME' => 604800,
-
-     // ENCRYPTION
-     'ENCRYPTION_KEY' => 'D7gÊìcL6f$*+&C$f^1x',
-     'HMAC_SALT' => '88z7nk#10q5t9c^4L6dM%',
-
- );
-
-
-?>
+);
--- core/Config.php
+++ core/Config.php
@@ -5,21 +5,21 @@ class Config
 {
     public static $config;
 
+    public static function init()
+    {
+        self::read();
+
+        $custom_config_file = self::$config['PATH_CONFIG'];
+        if (file_exists($custom_config_file)) {
+            $custom_config = require $custom_config_file;
+            foreach ($custom_config as $key => $value) {
+                self::$config[$key] = $value;
+            }
+        }
+    }
+
     public static function get($key)
     {
-        if (!self::$config) {
-            self::$config = self::read();
-
-            $custom_config_file = self::$config['PATH_CONFIG'];
-            if (file_exists($custom_config_file)) {
-                $custom_config = require $custom_config_file;
-                foreach ($custom_config as $key => $value) {
-                    self::$config[$key] = $value;
-                }
-            }
-
-        }
-
         return self::$config[$key];
     }
 
@@ -27,7 +27,7 @@ class Config
     {
         $base_url = '127.0.0.1';
 
-        return array(
+        self::$config = array(
 
             'MAINTENANCE' => false,
 
@@ -41,16 +41,24 @@ class Config
             'SITE_TITLE' => 'The Bit Twisters Home',
             'SITE_DESCRIPT' => 'A simple PHP MVC Framework - Home',
             'SITE_KEYWORDS' => 'php, home, thebittwisters, mvc, framework',
-            'SITE_LOGO' => 'http://'.$base_url.'/img/home.png',
-            'SITE_PREVIEW' => 'http://'.$base_url.'/img/home-preview.jpg',
-            'SITE_TERMS' => 'http://'.$base_url.'/about/terms/',
-            'SITE_PRIVACY' => 'http://'.$base_url.'/about/privacy/',
+            'SITE_LOGO' => 'http:/'.$base_url.'/img/home.png',
+            'SITE_PREVIEW' => 'http:/'.$base_url.'/img/home-preview.jpg',
+            'SITE_TERMS' => 'http:/'.$base_url.'/about/terms/',
+            'SITE_PRIVACY' => 'http:/'.$base_url.'/about/privacy/',
 
             'PATH_CONFIG' => realpath(dirname(__FILE__).'/../') . '/config.php',
             'PATH_APPS' => realpath(dirname(__FILE__).'/../') . '/apps/',
 
             'DEFAULT_APP' => 'home',
             'DEFAULT_ACTION' => 'index',
+
+            'DB_TYPE' => 'mysql',
+            'DB_HOST' => '*',
+            'DB_NAME' => 'test',
+            'DB_USER' => 'admin',
+            'DB_PASS' => 'UndefinedPassword',
+            'DB_PORT' => '3306',
+            'DB_CHARSET' => 'utf8mb4',
 
             'COOKIE_RUNTIME' => 604800,
             'COOKIE_PATH' => '/',
