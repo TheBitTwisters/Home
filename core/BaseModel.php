6ff2de2aca90419ff737614bcc8ed37a13b8e111
Michael Angelo <michael01angelo@gmail.com>
2019-05-22 07:14:33 +0800

    core BaseModel

---
+++ core/BaseModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Home;
+class BaseModel
+{
+    public $db;
+
+    public function __construct()
+    {
+        if (!$this->$db) {
+            try {
+                $timezone_setting = "SET time_zone='".Config::get('SITE_TIMEZONE')."'";
+                $options = [
+                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ,
+                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_WARNING,
+                    \PDO::MYSQL_ATTR_INIT_COMMAND => $timezone_setting
+                ];
+                $this->db = new \PDO (
+                    'mysql' .
+                    ':host=' . Config::get('DB_HOST') .
+                    ';dbname=' . Config::get('DB_NAME') .
+                    ';port=' . Config::get('DB_PORT') .
+                    ';charset=' . Config::get('DB_CHARSET'),
+                    Config::get('DB_USER'), Config::get('DB_PASS'), $options
+                );
+            } catch (\PDOException $e) {
+                $code = $e->getCode();
+                Redirect::to('error/db/'.$code);
+                exit();
+            }
+        }
+    }
+
+    private
+
+
+}
