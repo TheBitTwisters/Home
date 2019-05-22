28ad0e596936b868efa89634e2ad3225fec715b8
Michael Angelo <michael01angelo@gmail.com>
2019-05-23 07:08:33 +0800

    Fixed bootstrap viewport and added colors.css

--- apps/Home/view/styles.php
+++ apps/Home/view/styles.php
@@ -1,2 +1,4 @@
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="stylesheet" href="<?=$this->config('URL_PLUGINS')?>bootstrap/4/css/bootstrap.min.css">
 <link rel="stylesheet" href="<?=$this->config('URL_PLUGINS')?>fontawesome/5/css/all.min.css">
+<link rel="stylesheet" href="<?=$this->config('URL')?>css/colors.css">
