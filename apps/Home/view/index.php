b0ea428eafeb9166f4b51d600a4c0583ffd87b37
Michael Angelo <michael01angelo@gmail.com>
2019-05-23 07:08:57 +0800

    Home index view changes

--- apps/Home/view/index.php
+++ apps/Home/view/index.php
@@ -1,31 +1,43 @@
 <!DOCTYPE html>
-<html lang="<?=$this->config('SITE_LOCALE')?>" prefix="og: http://ogp.me/ns#">
+<html lang="<?=$this->config('SITE_LOCALE')?>" prefix="og: http://ogp.me/ns#" class="h-100">
 <head>
 <title>Home &mdash; <?=$this->config('SITE_TITLE')?></title>
 <?php $this->render('home/styles'); ?>
+<style media="screen">
+    body
+    {
+        background: #c7b58c;
+        background-image: url('<?=$this->config('SITE_PREVIEW')?>');
+        background-size: cover;
+        background-position: center;
+    }
+</style>
 </head>
-<body>
-<div class="container">
-    <main>
-        <div class="card bg-light mb-3">
-            <div class="card-body">
-                <div class="row justify-content-center">
-                    <div class="col-sm-8 col-md-6">
-                        <div class="lead text-center">
-                            <p class="card-text">
-                                <i class="fas fa-unlink fa-2x"></i>
-                            </p>
-                            <p class="card-text">
-                                This is home
-                            </p>
-                         </div>
-                    </div>
-                </div>
-            </div>
+<body class="h-100">
+    <div class="container-fluid">
+        <div class="fixed-bottom">
+            <p>
+                <a href="<?=$this->config('SNS_FACEBOOK')?>" class="colors-facebook" target="_blank">
+                    <span class="fa-stack fa-lg">
+                        <i class="fas fa-circle fa-stack-2x"></i>
+                        <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                    </span>
+                </a>
+                <a href="<?=$this->config('SNS_INSTAGRAM')?>" class="colors-instagram" target="_blank">
+                    <span class="fa-stack fa-lg">
+                        <i class="fas fa-circle fa-stack-2x"></i>
+                        <i class="fab fa-instagram fa-stack-1x fa-inverse"></i>
+                    </span>
+                </a>
+                <a href="<?=$this->config('SNS_YOUTUBE')?>" class="colors-youtube" target="_blank">
+                    <span class="fa-stack fa-lg">
+                        <i class="fas fa-circle fa-stack-2x"></i>
+                        <i class="fab fa-youtube fa-stack-1x fa-inverse"></i>
+                    </span>
+                </a>
+            </p>
         </div>
-    </main>
-</div>
+    </div>
 <?php $this->render('home/scripts'); ?>
 </body>
 </html>
-<?php $this->printData(); ?>
