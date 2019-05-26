0ccf0631daf07b122512952d069514d0d4bde670
Michael Angelo <michael01angelo@gmail.com>
2019-05-27 07:06:41 +0800

    Update for pfs

--- apps/Home/view/index.php
+++ apps/Home/view/index.php
@@ -41,3 +41,42 @@
 <?php $this->render('home/scripts'); ?>
 </body>
 </html>
+<!DOCTYPE html>
+<html lang="<?=$this->config('SITE_LOCALE')?>" prefix="og: http://ogp.me/ns#">
+    <head>
+<?php $this->render('analytics'); ?>
+        <title><?=$this->config('SITE_NAME')?> - <?=$this->config('SITE_CAPTION')?></title>
+<?php $this->render('default_ogp'); ?>
+<?php $this->render('required_meta'); ?>
+<?php $this->render('seo'); ?>
+    </head>
+    <body>
+<?php $this->render('fb_script'); ?>
+        <div class="container">
+<?php $this->render('header'); ?>
+            <div class="jumbotron">
+                <h1 class="title">Paradise Funeral Services</h1>
+                <p class="slogan">Your loved ones' paradise resting in peace</p>
+            </div>
+            <main>
+                <div class="row">
+                    <div class="col-md-8">
+<?php $this->render('quotes/message'); ?>
+                        <h3 class="content-title">You can trust Us</h3>
+                        <p class="content-text">
+                            <?= $this->company->profile ?>
+                        </p>
+                        <h3 class="content-title">What&apos;s new?</h3>
+<?php $this->render('news/list'); ?>
+                    </div>
+                    <div class="col-md-4">
+<?php $this->render('offers/sidenav'); ?>
+                    </div>
+                </div>
+            </main>
+<?php $this->render('footer'); ?>
+        </div>
+<?php $this->render('scripts'); ?>
+<?php $this->render('adsense'); ?>
+    </body>
+</html>
