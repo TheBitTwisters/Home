4512fdde8cbb191f1bd0b4882cfa16fcc02ce3bc
Michael Angelo <michael01angelo@gmail.com>
2019-05-27 14:37:44 +0800

    Removed unnecessary config

--- core/Config.php
+++ core/Config.php
@@ -36,10 +36,9 @@ class Config
 
             'SITE_LOCALE' => 'en_US',
             'SITE_TIMEZONE' => '+00:00',
-            'SITE_NAME' => 'The Bit Twisters Home',
+            'SITE_TITLE' => 'The Bit Twisters Home',
             'SITE_AUTHOR' => 'The Bit Twisters',
             'SITE_CAPTION' => 'A simple PHP MVC Framework - Home',
-            'SITE_TITLE' => 'The Bit Twisters Home',
             'SITE_DESCRIPT' => 'A simple PHP MVC Framework - Home',
             'SITE_KEYWORDS' => 'php, home, thebittwisters, mvc, framework',
             'SITE_LOGO' => 'http:/'.$base_url.'/img/home.png',
@@ -67,9 +66,6 @@ class Config
             'COOKIE_SECURE' => true,
             'COOKIE_HTTP' => true,
             'SESSION_RUNTIME' => 604800,
-
-            'ENCRYPTION_KEY' => 'D7gÊìcL6f$*+&C$f^1x',
-            'HMAC_SALT' => '88z7nk#10q5t9c^4L6dM%',
 
         );
     }
