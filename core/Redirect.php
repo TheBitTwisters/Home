e3377b7b17ce2947f7363c72850dfdc389c892fc
Michael Angelo <michael01angelo@gmail.com>
2019-07-23 19:30:33 +0800

    Auth passes unauthorized access to redirect login

--- core/Redirect.php
+++ core/Redirect.php
@@ -1,12 +1,19 @@
 <?php
 
 namespace Home;
+
 class Redirect
 {
 
     public static function home()
     {
         header("location: " . Config::get('URL'));
+    }
+
+    public static function login()
+    {
+        $login_url = Config::get('DEFAULT_LOGIN_URL');
+        header("location: " . Config::get('URL_ROOT') . $login_url);
     }
 
     public static function to($path)
