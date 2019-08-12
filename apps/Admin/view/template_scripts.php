50f3bcfd9bce1f22704b4387a429e2e4a79a82b0
Michael Angelo <michael01angelo@gmail.com>
2019-08-12 15:40:40 +0800

    Clean admin template scripts

--- apps/Admin/view/template_scripts.php
+++ apps/Admin/view/template_scripts.php
@@ -1,2 +1,1 @@
-<script src="<?=$this->config('URL_PLUGINS')?>/jquery/jquery-3.3.1.min.js"></script>
-<script src="<?=$this->config('URL_PLUGINS')?>/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+
