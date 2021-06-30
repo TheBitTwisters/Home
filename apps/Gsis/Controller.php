419b3add5214823f22a880c3f6aaeb911bf39f8b
Michael Angelo <michael01angelo@gmail.com>
2021-06-30 13:12:56 +0800

    Create Controller.php

---
+++ apps/Gsis/Controller.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Gsis;
+
+use \Home\RenderType as RenderType;
+use \Home\Request as Request;
+use \Home\Mail as Mail;
+use \Home\Config as Config;
+use \Home\Feedback as Feedback;
+
+class Controller extends \Home\BaseController
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+  	{
+        $data = [];
+
+        $this->renderJSON($data);
+    		return RenderType::OK;
+  	}
+
+    public function login()
+  	{
+        $data = [
+          'token' => 'AeHPvhg2Tqx6t83LkQMbZGaNFJnKWV9X'
+        ];
+
+        $this->renderJSON($data);
+    		return RenderType::OK;
+  	}
+
+    public function fetchUserdata()
+    {
+      $data = [
+        'id' => 'admin',
+        'name' => 'Dehny Ahn',
+        'telephone' => '+123 456 789',
+        'email' => 'email@email.com',
+        'address' => 'Seoul'
+      ];
+
+      $this->renderJSON($data);
+      return RenderType::OK;
+    }
+
+    public function check_notices()
+    {
+      $data = [
+        'notices' => [
+          'id' => 1,
+          'title' => 'Notice 1',
+          'content' => 'Notice 1 Sample content'
+        ],
+        'notices' => [
+          'id' => 2,
+          'title' => 'Notice 2',
+          'content' => 'Notice 2 Sample content'
+        ],
+      ];
+
+      $this->renderJSON($data);
+      return RenderType::OK;
+    }
+}
